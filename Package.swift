584f5fc87149792fb1c7e36bf4b4d174850bfe1c
Ainur <abaizhumanova@beeline.kz>
2021-03-01 14:22:11 +0600

    edit podspec

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .library(name: "MessageKit", targets: ["MessageKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nathantannar4/InputBarAccessoryView", .upToNextMajor(from: "5.2.3"))
+        .package(url: "https://github.com/nathantannar4/InputBarAccessoryView", .exact("4.3.3"))
     ],
     targets: [
         .target(

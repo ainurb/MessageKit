3785d86e1794039c709164598affdab6709a1b11
Ainur <abaizhumanova@beeline.kz>
2021-03-04 22:38:27 +0600

    fix: bumdle

--- Sources/Extensions/Bundle+Extensions.swift
+++ Sources/Extensions/Bundle+Extensions.swift
@@ -27,18 +27,19 @@ import UIKit
 
 internal extension Bundle {
 
-    static func messageKitAssetBundle() -> Bundle {
-        let podBundle = Bundle(for: MessagesViewController.self)
-        
-        guard let resourceBundleUrl = podBundle.url(forResource: "MessageKitAssets", withExtension: "bundle") else {
-            fatalError(MessageKitError.couldNotCreateAssetsPath)
-        }
-        
-        guard let resourceBundle = Bundle(url: resourceBundleUrl) else {
-            fatalError(MessageKitError.couldNotLoadAssetsBundle)
-        }
-        
-        return resourceBundle
-    }
+    static var messageKitAssetBundle: Bundle = Bundle.module
+//    static func messageKitAssetBundle() -> Bundle {
+//        let podBundle = Bundle(for: MessagesViewController.self)
+//
+//        guard let resourceBundleUrl = podBundle.url(forResource: "MessageKitAssets", withExtension: "bundle") else {
+//            fatalError(MessageKitError.couldNotCreateAssetsPath)
+//        }
+//
+//        guard let resourceBundle = Bundle(url: resourceBundleUrl) else {
+//            fatalError(MessageKitError.couldNotLoadAssetsBundle)
+//        }
+//
+//        return resourceBundle
+//    }
 
 }
